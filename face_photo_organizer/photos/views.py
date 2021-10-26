031cf41d1be5bbbdbd9a9b4c0628f924cd06e116
Sungoh Park <spark449062@gmail.com>
2021-10-26 02:01:02 -0600

    some html added

--- face_photo_organizer/photos/views.py
+++ face_photo_organizer/photos/views.py
@@ -4,8 +4,16 @@ from django.shortcuts import render
 
 def index(request): 
     photos = [
-        { 'name': 'picture01'},
-        { 'name': 'picture02'}
+        { 
+            'description': 'picture01',
+            'faces_found': 2, 
+            'slug': 'picture-01'
+        },
+        { 
+            'description': 'picture02', 
+            'faces_found': 1, 
+            'slug': 'picture-02'
+        }
     ]
     
     return render(request, 'photos/index.html', {
