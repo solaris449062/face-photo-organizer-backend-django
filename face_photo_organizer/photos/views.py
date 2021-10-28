ae626abf3156c9396dd984a36e6ff0a189dccf1b
Sungoh Park <spark449062@gmail.com>
2021-10-28 02:56:41 -0600

    file upload form

--- face_photo_organizer/photos/views.py
+++ face_photo_organizer/photos/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
 from .models import Photo
+from django.views import View
+from django.http import HttpResponseRedirect
 
 # from django.http import HttpResponse
+
+def save_uploaded_file(file):
+    with open("temp/test.jpg", "wb+") as destination:
+        for chunk in file.chunks():
+            print(1)
+            print(chunk)
+            destination.write(chunk)
+class CreatePhotoView(View):
+    def get(self, request):
+        return render(request, "photos/index.html")
+
+    def post(self, request):
+        save_uploaded_file(request.FILES["image"])
+        return HttpResponseRedirect("/photos/")
 
 def index(request): 
     photos = Photo.objects.all()
